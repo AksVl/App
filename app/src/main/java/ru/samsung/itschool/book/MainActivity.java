bc1004a95a24ce5a6d46f5a587c10d34c465d700
AksVl <vlad.axxxxxx@gmail.com>
2022-11-17 23:08:17 +0700

    InitialCommit

--- app/src/main/java/ru/samsung/itschool/book/MainActivity.java
+++ app/src/main/java/ru/samsung/itschool/book/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.sql.SQLOutput;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,5 +17,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    public void run(View view) {
+        EditText InputA = (EditText) findViewById(R.id.a);
+        EditText InputB = (EditText) findViewById(R.id.b);
+        EditText InputC = (EditText) findViewById(R.id.c);
+        double a = Double.parseDouble(InputA.getText().toString());
+        double b = Double.parseDouble(InputB.getText().toString());
+        double c = Double.parseDouble(InputC.getText().toString());
+        String result = null;
+        TextView textView = findViewById(R.id.res);
+        double D = b*b-4*a*c;
+        if(D==0 && !(a == 0 && b==0 && c==0)){
+            result = Double.toString((-1*b)/(2*a));
+        }
+        else if(D<0 && a != 0){
+            result = "none";
+        }
+        else if(D>0 && a != 0){
+            result = ((-1*b-Math.sqrt(D))/(2*a))+" "+((-1*b+Math.sqrt(D))/(2*a));
+        }
+        else if(a == 0 && b==0 && c==0){
+            result = "any";
+        }
+        else if(a==0){
+            result = Double.toString(-1*c/b);
+        }
+        textView.setText(result);
+    }
 }
